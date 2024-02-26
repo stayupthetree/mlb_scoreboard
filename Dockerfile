a973a143e527d02088b636f7e817095a5f8b2b44
NHL LED Scoreboard img <pi@scoreboard>
2024-02-26 12:30:02 -0500

    fixes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use the official Debian slim image as the base image
-FROM debian:12-slim
+FROM python:3.9-bookworm
 
 # Install system dependencies
 RUN apt-get update && apt-get install -y \
@@ -8,30 +8,17 @@ RUN apt-get update && apt-get install -y \
     python3 \
     inotify-tools \
     python3-pip \
+    python3-dev \
     python3-venv \
     cmake \
     build-essential \
     libssl-dev \
     libffi-dev \
-    python3-dev \
     libsdl2-dev \
-    libsdl2-image-dev \
-    libsdl2-mixer-dev \
-    libsdl2-ttf-dev \
-    libportmidi-dev \
-    libswscale-dev \
-    libavformat-dev \
-    libavcodec-dev \
-    python3-dev \
     python3-pillow \
     python3-tk \
     libxml2-dev \
     libxslt-dev \
-    libsdl2-mixer-2.0-0 \
-    libsdl2-image-2.0-0 \
-    libsdl2-2.0-0 \
-    libsdl2-ttf-2.0-0 \
-    libopenjp2-7 \
     && rm -rf /var/lib/apt/lists/*
 
 # Set the working directory in the container to /app
@@ -48,6 +35,7 @@ RUN python3 -m venv venv
 ENV PATH="/app/mlb-led-scoreboard/venv/bin:$PATH"
 
 # Install the specified Python packages
+RUN pip3 install wheel
 RUN pip3 install --no-cache-dir feedparser==6.0.10 'MLB_StatsAPI>=1.6.1' pyowm==3.3.0 'RGBMatrixEmulator>=0.8.4' tzlocal==4.2
 
 # Clone and install the rpi-rgb-led-matrix library
@@ -63,13 +51,12 @@ RUN echo "Running rgbmatrix installation..." && \
 COPY entrypoint.sh /app/mlb-led-scoreboard/entrypoint.sh
 RUN chmod +x /app/mlb-led-scoreboard/entrypoint.sh
 
-# Create a non-root user and switch to it
+## Create a non-root user and switch to it
 RUN useradd -m dockeruser && echo "dockeruser ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/dockeruser
 
 # It's important to switch to the non-root user after all commands that require root access have been executed
 #COPY watcher.sh /watcher.sh
 #RUN chmod +x /watcher.sh
-
 
 
 ENTRYPOINT ["/app/mlb-led-scoreboard/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Default WATCHER_ENABLED to false if not set
+WATCHER_ENABLED=${WATCHER_ENABLED:-false}
+
+echo "Watcher Enabled: $WATCHER_ENABLED" # Debug line to check the environment variable
+
 # Define the command to start main.py
 main_command="python3 /app/mlb-led-scoreboard/main.py"
 
@@ -13,11 +18,15 @@ start_main_py() {
         fi
     done < <(env)
 
-    # Redirect output of main.py to stdout/stderr of PID 1
+    # Echo the full command for debugging
+    echo "Running command: $main_command $additional_params"
+
+    # Redirect output of main.py to stdout/stderr of PID 1 and execute it
     $main_command $additional_params > /proc/1/fd/1 2>/proc/1/fd/2 &
     MAIN_PY_PID=$!
 }
 
+# Function to kill main.py specifically
 kill_main_py() {
     if [ ! -z "$MAIN_PY_PID" ]; then
         echo "Killing the existing main.py process..."
@@ -25,20 +34,34 @@ kill_main_py() {
     fi
 }
 
+# Function to start the watcher
 start_watcher() {
-    inotifywait -m -e modify -e move -e create -e delete "/app/mlb-led-scoreboard/config.json" "/app/mlb-led-scoreboard/coordinates/" "/app/mlb-led-scoreboard/colors/" |
-    while read -r directory events filename; do
-        # Extract just the filename from the path
-        just_filename=$(basename "${directory}${filename}")
-        echo "Config change detected $just_filename. The MLB Scoreboard will restart."
-        kill_main_py
-        sleep 1
-        start_main_py
-    done
+    # Check if watcher is enabled
+    if [ "$WATCHER_ENABLED" = "true" ]; then
+        inotifywait -m -e modify -e move -e create -e delete "/app/mlb-led-scoreboard/config.json" "/app/mlb-led-scoreboard/coordinates/" "/app/mlb-led-scoreboard/colors/" |
+        while read -r directory events filename; do
+            # Extract just the filename from the path
+            just_filename=$(basename "${directory}${filename}")
+            echo "Config change detected $just_filename. The MLB Scoreboard will restart."
+            kill_main_py
+            sleep 1
+            start_main_py
+        done
+    else
+        echo "Watcher is disabled."
+    fi
 }
 
+# Kill any existing instances of main.py
 kill_main_py
-start_main_py
-start_watcher &
 
+# Start main.py
+start_main_py
+
+# Start the watcher
+if [ "$WATCHER_ENABLED" = "true" ]; then
+    start_watcher &
+fi
+
+# Wait indefinitely to prevent the container from exiting
 wait
