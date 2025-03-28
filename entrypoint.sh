234738bbe218a7bab4b3627967b440cc06660f6f
stayupthetree <stayupthetree@gmail.com>
2025-03-28 09:05:44 -0400

    Testing web browser emulation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
-
 # Default WATCHER_ENABLED to false if not set.
 WATCHER_ENABLED=${WATCHER_ENABLED:-false}
-
+# Default EMULATED to false if not set
+EMULATED=${EMULATED:-false}
 # Retrieve UID and GID from environment variables, defaulting to 1000 if not provided
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 
 echo "Watcher Enabled: $WATCHER_ENABLED" # Debug line to check the environment variable.
+echo "Emulated Mode: $EMULATED" # Debug line to confirm emulation mode
 echo "Using PUID: $PUID, PGID: $PGID" # Debug line to confirm PUID and PGID
 
 # Function to copy initial example files to /app/configs if not already done
 copy_initial_configs() {
     local flag_file="/app/configs/.initial_copy_done"
-
     if [ ! -f "$flag_file" ]; then
         if [ -f "/app/config.json.example" ]; then
             cp -n "/app/config.json.example" "/app/configs/config.json.example"
             chown $PUID:$PGID "/app/configs/config.json*"
         fi
-
         if [ -f "/app/colors/scoreboard.json" ]; then
             cp -n "/app/colors/scoreboard.json" "/app/configs/scoreboard.json.example"
             chown $PUID:$PGID "/app/configs/scoreboard.json*"
         fi
-
         if [ -f "/app/colors/teams.json.example" ]; then
             cp -n "/app/colors/teams.json.example" "/app/configs/teams.json.example"
             chown $PUID:$PGID "/app/configs/teams.json*"
         fi
-
         for file in /app/coordinates/*.json /app/coordinates/*.json.example; do
             if [ -f "$file" ]; then
                 cp -n "$file" "/app/configs/"
@@ -52,7 +49,6 @@ copy_specific_files() {
         cp -f "/app/configs/config.json" "/app/"
         chown $PUID:$PGID "/app/config.json"
     fi
-
     if [ -f "/app/configs/scoreboard.json" ]; then
         if [ -f "/app/colors/scoreboard.json" ]; then
             mv -f "/app/colors/scoreboard.json" "/app/colors/scoreboard.json.bak"
@@ -61,7 +57,6 @@ copy_specific_files() {
         cp -f "/app/configs/scoreboard.json" "/app/colors/"
         chown $PUID:$PGID "/app/colors/scoreboard.json"
     fi
-
     if [ -f "/app/configs/teams.json" ]; then
         if [ -f "/app/colors/teams.json" ]; then
             mv -f "/app/colors/teams.json" "/app/colors/teams.json.bak"
@@ -70,7 +65,6 @@ copy_specific_files() {
         cp -f "/app/configs/teams.json" "/app/colors/"
         chown $PUID:$PGID "/app/colors/teams.json"
     fi
-
     for file in /app/configs/*.json; do
         filename=$(basename "$file")
         if [[ $filename =~ w(32|64|128)h(32|64).json(.example|.sample)? ]]; then
@@ -92,15 +86,21 @@ main_command="python3 /app/main.py"
 
 # Function to start main.py
 start_main_py() {
-#    echo "Starting MLB Scoreboard..."
     additional_params=""
+    
+    # Add emulated parameter if enabled
+    if [ "$EMULATED" = "true" ]; then
+        additional_params+="--emulated "
+    fi
+    
+    # Process other parameters from environment variables
     while IFS='=' read -r name value ; do
         if [[ $name == PARAM_* ]]; then
             param_name=$(echo "$name" | sed -e 's/^PARAM_//g' | tr '[:upper:]' '[:lower:]' | sed -e 's/_/-/g')
             additional_params+="--$param_name=$value "
         fi
     done < <(env)
-
+    
     # Start main.py with additional parameters and capture its PID
     chown -R $PUID:$PGID "/app/configs/"
     $main_command $additional_params > /proc/1/fd/1 2>/proc/1/fd/2 &
